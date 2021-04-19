83f0de96a269e5915e6d87819738466970690b69
João Pedro de Oliveira Ferreira <jpdeoliveiraferreira@gmail.com>
2021-04-19 16:27:32 -0300

    Adicionando models Pedido e ItemPedido

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+from products.models import Product
+from uuid import uuid4
 
-# Create your models here.
+class Order(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False, unique=True)
+    user = models.ForeignKey(User,on_delete= models.CASCADE)
+    price = models.FloatField()
+    status = models.CharField(
+        max_length=2,
+        choices = (
+            ('C', 'Completed'),
+            ('AP','Awaiting Payment'),
+            ('AS','Awaiting Shipment'),
+            ('S','Shipped'),
+            ('CN','Cancelled')
+        )
+    )
+    def __str__(self):
+        return str(self.id)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+
+
+
