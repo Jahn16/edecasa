bb34270ad63cda13b6cafed7329a4fa78b07ec0c
João Pedro de Oliveira Ferreira <jpdeoliveiraferreira@gmail.com>
2021-04-19 18:53:10 -0300

    Adicionando model Conta

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Account(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    date_of_birth = models.DateField()
+    cpf = models.PositiveBigIntegerField(max_length=11)
+    address = models.CharField(max_length=50)
+    house_number = models.CharField(max_length=30)
+    address_complement = models.CharField(max_length=255)
+    cep = models.PositiveIntegerField(max_length=8)
+    def __str__(self):
+        return self.user.username
